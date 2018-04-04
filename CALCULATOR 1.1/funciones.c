ece56ddd26fdd074e159c422db4e2efd52cb7c7c
Lautaro01 <36265003+Lautaro01@users.noreply.github.com>
2018-04-03 21:20:32 -0300

    calculadora

---
+++ CALCULATOR 1.1/funciones.c
@@ -0,0 +1,15 @@
+
+#ifndef FUNCIONES_H_INCLUDED
+#define FUNCIONES_H_INCLUDED
+
+int suma (int,int);
+
+int resta (int,int);
+
+float division (float,float);
+
+int mulplicacion (int,int);
+
+int factorial(int);
+
+#endif // FUNCIONES_H_INCLUDED
