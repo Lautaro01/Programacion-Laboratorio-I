4f113e9bca3e76ad46f1e3f139fb939b78902070
Lautaro01 <36265003+Lautaro01@users.noreply.github.com>
2018-06-06 12:26:44 -0300

    Add files via upload

---
+++ hoy/Windows_64/main.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "ArrayList.h"
+#include "Employee.h"
+
+/****************************************************
+    Menu:
+        1. Parse del archivo data.csv
+        2. Listar Empleados
+        3. Ordenar por nombre
+        4. Agregar un elemento
+        5. Elimina un elemento
+        6. Listar Empleados (Desde/Hasta)
+*****************************************************/
+
+
+int main()
+{
+    char seguir = 's';
+    int opcion;
+    do
+    {
+        printf("M E N U\n\n");
+        printf("1. Parse del archivo data.csv\n");
+        printf("2. Listar Empleados\n");
+        printf("3. Ordenar por nombre\n");
+        printf("4. Agregar un elemento\n");
+        printf("5. Elimina un elemento\n");
+        printf("6. Listar Empleados (Desde/Hasta)\n\n");
+        printf("Opcion: ");
+        opcion = getch();
+
+        switch(opcion)
+        {
+        case 1:
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        case 5:
+            break;
+        case 6:
+            seguir='n';
+            break;
+        default
+            printf("");
+        }
+
+
+    }while(seguir != 'n')
+
+    return 0;
+}
